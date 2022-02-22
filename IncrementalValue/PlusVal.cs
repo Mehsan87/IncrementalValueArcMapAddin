9bff1275f49059d8d2867ee75f9d5a7ed383dd24
Mehsan <mehsanhaque87@gmail.com>
2022-02-22 15:07:15 +0600

    Add project files.

---
+++ IncrementalValue/PlusVal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace IncrementalValue
+{
+    public class PlusVal : ESRI.ArcGIS.Desktop.AddIns.Tool
+    {
+        public PlusVal()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
